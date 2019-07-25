d275beb06398806248c961048fe3bd0e9d18ac66
doubleface <christophe@cozycloud.cc>
2019-07-25 12:58:42 +0200

    chore: upgrade webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 const fs = require('fs')
 const SvgoInstance = require('svgo')
 
 const entry = require('./package.json').main
 
+const readManifest = () =>
+  JSON.parse(fs.readFileSync(path.join(__dirname, './manifest.konnector')))
+
 const svgo = new SvgoInstance({
   plugins: [
     {
-      inlineStyles: {
-        onlyMatchedOnce: false
-      }
+      inlineStyles: { onlyMatchedOnce: false }
     }
   ]
 })
@@ -33,22 +35,6 @@ module.exports = {
     path: path.join(__dirname, 'build'),
     filename: 'index.js'
   },
-
-  module: {
-    // to ignore the warnings like :
-    // WARNING in ../libs/node_modules/bindings/bindings.js 76:22-40
-    // Critical dependency: the request of a dependency is an expression
-    // Since we cannot change this dependency. I think it won't hide more important messages
-    exprContextCritical: false,
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|cozy-(bar|client-js))/,
-        loader: 'babel-loader'
-      }
-    ]
-  },
-
   plugins: [
     new CopyPlugin([
       { from: 'manifest.konnector' },
@@ -57,8 +43,18 @@ module.exports = {
       { from: 'assets', transform: optimizeSVGIcon },
       { from: '.travis.yml' },
       { from: 'LICENSE' }
-    ])
-  ]
+    ]),
+    new webpack.DefinePlugin({
+      __WEBPACK_PROVIDED_MANIFEST__: JSON.stringify(readManifest())
+    })
+  ],
+  module: {
+    // to ignore the warnings like :
+    // WARNING in ../libs/node_modules/bindings/bindings.js 76:22-40
+    // Critical dependency: the request of a dependency is an expression
+    // Since we cannot change this dependency. I think it won't hide more important messages
+    exprContextCritical: false
+  }
 }
 
 function optimizeSVGIcon(buffer, path) {
